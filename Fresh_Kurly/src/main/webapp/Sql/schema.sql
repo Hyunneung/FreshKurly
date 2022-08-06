f2559e00ad358600991859abaed6398191aa416d
최시준 <83524187+ahslxj1993@users.noreply.github.com>
2022-08-06 10:45:25 +0900

    Sijoon (#84)
    * practice라는 사용자를 사용할거라서 안내문을 바꿨어요

--- Fresh_Kurly/src/main/webapp/Sql/schema.sql
+++ Fresh_Kurly/src/main/webapp/Sql/schema.sql
@@ -1,5 +1,5 @@
-CREATE USER semi IDENTIFIED BY 1234;  
+--practice 라는 사용자를 사용할거에요
+CREATE USER practice IDENTIFIED BY 1234;  
 --권한 부여
-GRANT RESOURCE, CONNECT TO semi; 
-
+GRANT RESOURCE, CONNECT TO practice; 
 --시스템 계정에서 사용해야 하니까 cmd 창에서 사용하세요
